c412ce0e1f829bf459edbe26487d6a949fe40f80
Srinath Natarajan <sriyegna@gmail.com>
2020-12-10 18:42:00 -0500

    Add min and maxes

--- routes/mobility.js
+++ routes/mobility.js
@@ -36,23 +36,41 @@ router.get("/apple", async (req, res) => {
       }
     }
 
+    const max = {
+      driving: Number.MIN_VALUE,
+      transit: Number.MIN_VALUE,
+      walking: Number.MIN_VALUE,
+    };
+
+    const min = {
+      driving: Number.MAX_VALUE,
+      transit: Number.MAX_VALUE,
+      walking: Number.MAX_VALUE,
+    };
+
     for (let i = 0; i < regionOptions.length; i++) {
       const region = regionOptions[i];
       walking = getAvg(regions[region].walking) - 100;
+      max.walking = max.walking < walking ? walking : max.walking;
+      min.walking = min.walking < walking ? min.walking : walking;
       regions[region].walking = walking
         ? (getAvg(regions[region].walking) - 100).toFixed(3)
         : null;
       transit = getAvg(regions[region].transit) - 100;
+      max.transit = max.transit < transit ? transit : max.transit;
+      min.transit = min.transit < transit ? min.transit : transit;
       regions[region].transit = transit
         ? (getAvg(regions[region].transit) - 100).toFixed(3)
         : null;
       driving = getAvg(regions[region].driving) - 100;
+      max.driving = max.driving < driving ? driving : max.driving;
+      min.driving = min.driving < driving ? min.driving : driving;
       regions[region].driving = driving
         ? (getAvg(regions[region].driving) - 100).toFixed(3)
         : null;
     }
 
-    return res.status(200).send(regions);
+    return res.status(200).send({ regions, max, min });
   } catch (err) {
     console.log(err);
     return res.status(500).send({ message: "Error getting data." });
